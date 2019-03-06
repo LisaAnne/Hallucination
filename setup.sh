76b82ebd437a430a6f2699b971ce68e4a79f5d0f
LisaAnne <lisa.a.hendricks@gmail.com>
2019-03-06 12:05:25 -0800

    download intermediate files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#Download generated sentences
 wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/generated_sentences.zip
 unzip generated_sentences.zip
 rm -r generated_sentences.zip
+
+#Download 
+mkdir output
+cd output
+wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/intermediate_image.zip
+unzip intermediate_image.zip
+rm -r intermediate_image.zip
+
+cd ../data
+wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/gt_labels.p
+wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/vocab.p
+
+cd ..
