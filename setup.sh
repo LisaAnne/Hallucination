cd718c5bc0dc7b8735b97e399543fb9f942f2edc
LisaAnne <lisa.a.hendricks@gmail.com>
2019-03-29 14:39:39 -0700

    download human scores

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ rm -r intermediate_image.zip
 cd data
 wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/gt_labels.p
 wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/vocab.p
-#wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/human_scores.zip
-#unzip human_scores.zip
-#rm -r human_scores.zip
+wget https://people.eecs.berkeley.edu/~lisa_anne/hallucination/human_scores.zip
+unzip human_scores.zip
+rm -r human_scores.zip
 
 cd ..
